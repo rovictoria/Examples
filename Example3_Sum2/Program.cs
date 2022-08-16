f827691171556053fc84fd7328fe195e24d5481d
Victoria R <vik-rozanova@yandex.ru>
2022-08-17 01:50:10 +0300

    UPD Example3: + WriteLine numbers too

--- Example3_Sum2/Program.cs
+++ Example3_Sum2/Program.cs
@@ -1,5 +1,7 @@
 ﻿int numberA = new Random().Next(1,101); // целое число из диапазона [1...100]
+Console.WriteLine(numberA);
 int numberB = new Random().Next(1,101); // также;;
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 // Можно обойтись без result, тогда в выводе написать действие numberA + numberB
 Console.WriteLine(result);
