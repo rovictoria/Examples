31b60fada7b35406d2cf68c0bddaf27cdd7cd811
Victoria R <vik-rozanova@yandex.ru>
2022-08-17 01:47:25 +0300

    UPD Example3: Sum 2 random numbers

--- Example3_Sum2/Program.cs
+++ Example3_Sum2/Program.cs
@@ -1,5 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,101); // целое число из диапазона [1...100]
+int numberB = new Random().Next(1,101); // также;;
 int result = numberA + numberB;
 // Можно обойтись без result, тогда в выводе написать действие numberA + numberB
 Console.WriteLine(result);
