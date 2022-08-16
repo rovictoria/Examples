a459c41de4c99f19c9529010d88cdf8391844c5b
Victoria R <vik-rozanova@yandex.ru>
2022-08-17 00:35:22 +0300

    Приветствовать пользователя добавлено

---
+++ Example2_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите ваше имя");
+// sting - строка, тип данных
+// считывание данных при помощи readLine
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
