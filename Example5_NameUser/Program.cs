b8e6392d3a86664ebd0941b61183cb914373b6a9
Victoria R <vik-rozanova@yandex.ru>
2022-08-17 21:38:08 +0300

    Example5 Helo user with if-else

---
+++ Example5_NameUser/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "вика")
+{
+  Console.WriteLine("Ура, это же ВИКА!!!");  
+}
+else
+{
+    Console.WriteLine("Привет, " + username);
+}
