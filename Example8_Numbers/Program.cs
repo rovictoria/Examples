4739e7bb57f5644e219f491754f013177c621f99
Victoria R <vik-rozanova@yandex.ru>
2022-08-18 21:33:53 +0300

    Example8 Square number if

---
+++ Example8_Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int value = numberB * numberB;
+
+if(value == numberA)
+{
+    Console.WriteLine(numberA + " является квадратом " + numberB);
+}
+else
+{
+    Console.WriteLine(numberA + " НЕ является квадратом " + numberB);
+}
